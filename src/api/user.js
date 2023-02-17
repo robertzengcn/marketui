9ef26b3bbdd2b6ab83c85bdee14437725ca7cee1
jianze_zeng <jianze_zeng@foxitsoftware.com>
2023-02-17 10:59:03 +0800

    update ignore

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,19 @@
 import request from '@/utils/request'
 
 export function login(data) {
+  var bodyFormData = new FormData()
+  bodyFormData.append('username', data.username)
+  bodyFormData.append('password', data.password)
   return request({
-    url: '/vue-element-admin/user/login',
+    url: '/login/accountlogin',
     method: 'post',
-    data
+    data: bodyFormData
   })
 }
 
 export function getInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
+    url: '/user/info',
     method: 'get',
     params: { token }
   })
